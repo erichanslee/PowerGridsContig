c57d66d5be237e864ab7cc0e131694607a555c92
ericlee0803 work <ericlee0803@yahoo.com>
2016-07-12 10:14:48 -0400

    forgot to commit few things

---
+++ 9bus/printnorms.m
@@ -0,0 +1,13 @@
+%%prints the norms of a matrix entry wise
+
+function printnorms(A)
+    [n,m] = size(A);
+    N = zeros(n,m);
+    for i = 1:n
+        for j = 1:m
+            N(i,j) = norm(A(i,j));
+        end
+    end
+    
+    display(N);
+end
