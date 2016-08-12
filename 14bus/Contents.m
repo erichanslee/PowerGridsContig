88ca2c7f166cef9b3bda19c9f68e31509ca58cc7
David Bindel <bindel@cs.cornell.edu>
2016-08-12 12:33:08 -0400

    Killed results.mat (some random 10-by-10 matrix?)

--- 14bus/Contents.m
+++ 14bus/Contents.m
@@ -4,7 +4,6 @@
 %   bus14.m      - PSAT file to specify 14 bus system
 %   contig*.m    - PSAT files for the contingency systems
 %   metadata.mat - Specify params for 14 bus case
-%   results.dat  - Results??  Not documented anywhere.  Just a 10-by-10
 %
 % Static analysis:
 %   genscript   - Generate data/ submatrix with power flow Jacobians
