412f25fcc6138f8199fa8e06f4c683f4a6de0cab
Eric Lee <ericlee0803@yahoo.com>
2016-09-18 13:15:38 -0400

    Added new runme for David

--- 14bus/runme.m
+++ 14bus/runme.m
@@ -1,79 +1,23 @@
-%% RUNME
+% Please run the following:
 
-% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% ~~~~~~~~~~~~~~~~~~~README~~~~~~~~~~~~~~~~~~~~~~~
-% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Method = 3;
+MatrixNum = 1; ContigNum = 1;
+Noise = 0; PMUidx = 1:14;
 
-%   SET UP: given (M-lambda*E) and a subset of a potential eigenvector x1,
-%   how best fit x_\2 to minimize || (M - lambda*E)[x1;x2] ||?
+[true_eigenvecs, fitted_eigenvecs, residual_vecs, dot_product] = isolatecase(Method, ContigNum, MatrixNum, Noise, PMUidx);
 
-%       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-%       Description of (Contingency Identification) Methods:
-%       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-%   Method 1 = least squares fit subject to || x1 || = 1
-
-%   Method 2 = least squares fit subject to || x1 || = 1 with a
-%   normalization after
-
-%   Method 3 = least squares fit subject to || [alpha*x1; x2] || = 1
-
-%   Method 4 = least squares fit subject to || [alpha*x1; x2] || = 1
-%   and then an reverse-normalization via scaling by 1/alpha to bring
-%   || x1 || = 1
+% Outputs should be self-explanatory: 
+% -true_eigenvecs calcualtes the generalized eigenvectors of the linearized system
+% -fitted_eigenvecs are empirical eigenvectors fitted from n4sid data
+% -residual_vecs are the vectors of residuals
+% -dot_product is the matrix of inner products between fitted and true eigenvecs
+% Note that the residual vectors are each quite small, but the dot products indicate that in fact the eigenvectors are not close to each other. 
 
 
-%       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-%        Important Things
-%       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-%   **Please don't set noise to anything but 0 right now it's not correctly
-%   implemented :) 
-
-%   **The numbering of the Methods is the same as the report with the
-%   exception that Method 4 is the modified Method 3.
-
-%   **Figures/plots will be saved in the "Figures" folder.
-
-%   ****RUN genscript14 FIRST BEFORE DOING ANYTHING ELSE AS IT GENERATES NECESSARY FILES****
-
-% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% ~~~~~~~~~~~~~~~~~END README~~~~~~~~~~~~~~~~~~~~~
-% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-% Just to make things clearer
-Method1 = 1; Method2 = 2; Method3 = 3; Method4 = 4; 
-load metadata.mat;
-
-% PMUs at all buses
-percentage = 1;
-noise = 0;
-idx = gen_PMUidx(percentage, numbuses);
-
-
-% Contig 3 is a case where misidentification occurs (Contig 4 identified instead)
-% NOTE: PMU data is on rows 64:77 of linearvecs/empvecs/empresidual
-
-rangebus = (differential + numlines + 1):(differential + numlines + numlines);
-
-% The output from the correctly-fitted data (that is "worse" by the averaging metric compared to
-% the false positive)
-method = Method3;
-contignum = 3; % Contingency to Simulate
-matrixnum = 3; % Jacobian in which to fit eigenvectors
-[linearvecs_true, empvecs_true, empresidual_true] = isolatecase(method, contignum, matrixnum, noise, idx);
-
-% The false positive 
-method = Method3;
-contignum = 3; % Contingency to Simulate
-matrixnum = 4; % Jacobian in which to fit eigenvectors
-[linearvecs_false, empvecs_false, empresidual_false] = isolatecase(method, contignum, matrixnum, noise, idx);
-clc;
-
-% For example, compare portions of false positive eigenvectors seen by buses: 
-pmode1 = 'entire';
-pmode2 = 'PMU';
-j = 2;
-printvecs(linearvecs_false, empvecs_false, j, pmode1);
-
+% Then run the unit_tests.m
+unit_tests
+% unit_tests.m simply runs the fitting procedure on a subset of generalized eigenvectors taken from the linearized system. You should be getting some
+% sort of warning, saying that we get an ill-conditioned problem. I'm not sure if this is truly a problem or not: the fitting from the empirical 
+% (n4sid) side of things doesn't seem to be a problem. But it might explain why the fit is not that great in the previous problem. 
--- 14bus/unit_tests.m
+++ 14bus/unit_tests.m
@@ -1,34 +1,39 @@
 % Checks correctness of id_contig.m
 
-%% Test Case: Random
+%% Test Case: Random (Works Fine)
 load metadata.mat
 
-n = differential + algebraic;
-A = rand(n);
-E = eye(n);
-method = 3;
-[v1,d1] = eig(A,E);
-win =  (differential + numlines + 1):(differential + numlines + numlines);
-v1_win = v1(win,:);
-d1_diag = diag(d1);
+% n = differential + algebraic;
+% A = rand(n);
+% E = eye(n);
+% method = 3;
+% [v1,d1] = eig(A,E);
+% win = (differential + numlines + 1):(differential + numlines + numlines);
+% v1_win = v1(win,:);
+% d1_diag = diag(d1);
+% 
+% [res1,vec1] = id_contig(A,E,method,d1_diag, normalizematrix(v1_win), win);
 
-[res1,vec1] = id_contig(A,E,method,d1_diag, normalizematrix(v1_win), win);
 
-
-%% Test Case: Power System
+%% Test Case: Power System (Doesn't work well)
 
 maxfreq = .5;
 minfreq = .05;
 contignum = 1;
+n = differential + algebraic;
 
 I = eye(differential);
 E = zeros(algebraic + differential);
 E(1:differential,1:differential) = I;
 A = full(matrix_read(sprintf('data/matrixfull%d', contignum)));
+win = (differential + numlines + 1):(differential + numlines + numlines);
 [v2,d2] = eig(A,E); 
-d2 = d2 + 1e-6*eye(size(E));
-
-
 [v2_subset, d2_subset] = filter_eigpairs(minfreq, maxfreq, diag(d2), v2);
 
-[res2, vec2] = id_contig(A, E, method, d2_subset, normalizematrix(v2_subset(win,:)), win);
+% Pick out window and normalize %
+v2_arg = normalizematrix(v2_subset(win,:));
+[res2, vec2] = id_contig(A, E, method, d2_subset, v2_arg, win);
+
+%% 
+disp('Dot Product of Fitted and Real eigenvector in Second Problem:');
+disp(vec1(:,2)'*v2(:,1)/norm(v2(:,1)));
